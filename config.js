ed0e59498973874bc7d1bc91d56416921a77c187
RIAN-AKBAR <rian032008@gmail.com>
2026-09-12 07:21:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.CONFIG = {
     links: {
-        registerMember: 'https://classroom-homeless-turbo-costumes.trycloudflare.com/register_asc',
-        adminLogin: 'https://classroom-homeless-turbo-costumes.trycloudflare.com/register_asc/login.php',
+        registerMember: 'https://pediatric-mod-editing-hugh.trycloudflare.com/register_asc',
+        adminLogin: 'https://pediatric-mod-editing-hugh.trycloudflare.com/register_asc/login.php',
         discord: 'https://discord.gg/7hJX3xMfD',
         waGroup: 'https://chat.whatsapp.com/FCiXQDejHtEC1s12z074dh?s=cl&p=a&ilr=1',
         waColab: '6285781209423',
