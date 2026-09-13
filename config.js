669275d5cdf874cb3792676224d8cdd505504b31
RIAN-AKBAR <rian032008@gmail.com>
2026-09-13 12:14:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.CONFIG = {
     links: {
-        registerMember: 'https://triumph-sophisticated-involvement-mrna.trycloudflare.com/register_asc',
-        adminLogin: 'https://triumph-sophisticated-involvement-mrna.trycloudflare.com/register_asc/login.php',
+        registerMember: 'https://beaches-adequate-maui-afternoon.trycloudflare.com/register_asc',
+        adminLogin: 'https://beaches-adequate-maui-afternoon.trycloudflare.com/register_asc/login.php',
         discord: 'https://discord.gg/7hJX3xMfD',
         waGroup: 'https://chat.whatsapp.com/FCiXQDejHtEC1s12z074dh?s=cl&p=a&ilr=1',
         waColab: '6283854980779',
